7b5116fc0dcdd12cda4ac14fc1fb6647593a806f
lai2 <informatica@lai2>
2020-02-26 08:39:52 +0100

    primo push di un progetto già iniziato

---
+++ rest_json/src/main/java/post.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.lang.String;
+
+public class post 
+{
+    int Id = 10;
+    String Nome = "pippo";
+    String Cognome = "pluto";
+    String Email = "Email";
+    String Telefono = "123456";
+    
+	// http://localhost:8080/api/tutorial/1.0/employees
+	public void post()
+        {
+
+	  try {
+
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		String input = "{ \\ \n" +
+                               "   \"employeeId\":" + this.Id +
+                               "   \"firstName\":" + this.Nome +
+                               "   \"lastName\":" + this.Cognome +
+                               "   \"email\":" + this.Email +
+                               "   \"phone\":" + this.Telefono +
+                               " }'";
+
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+
+		if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
+
+	}
+
+}
---
+++ rest_json/src/main/java/rest_js.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author informatica
+ */
+public class rest_js
+{
+    public static void main(String[] args) 
+        {
+             post pippo = new post();
+        pippo.post();
+        get tizzio = new get();
+        tizzio.get();
+       
+        }
+    }
